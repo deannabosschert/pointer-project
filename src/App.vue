31523eca99e748a405600604a9ec5ef0cd781797
Kris <kriskuiper74@gmail.com>
2019-12-16 11:12:41 +0100

    feat: dump vue boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <hello-world msg="Pointer project"/>
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/hello-world'
 
 export default {
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'Pointer!'
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: white;
 }
 </style>
