739b4be23b5d903a402d0663e197a85521a8a046
Kris <kriskuiper74@gmail.com>
2019-12-17 10:57:31 +0100

    feat: init ssr with nuxt

---
+++ pages/shop/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>This is the shop</h1>
+    <nuxt-link to="/">To homepage</nuxt-link>
+  </main>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+
+</style>
