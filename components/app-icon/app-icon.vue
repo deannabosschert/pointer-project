b381c93601d9a4b98831e8cd9a578f69749b2395
Kris <kriskuiper74@gmail.com>
2019-12-18 10:44:03 +0100

    feat: add app-icon

---
+++ components/app-icon/app-icon.vue
@@ -0,0 +1,25 @@
+<template>
+  <img
+    :src="`/assets/icons/${name}`"
+    alt=""
+    class="app-icon"
+  />
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    },
+  }
+</script>
+
+<style>
+  .app-icon {
+    height: 1em;
+    width: 1em;
+  }
+</style>
