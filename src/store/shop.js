86853ad52e0bb5b5153f514eff7bd6cb0a44a4ba
Kris <kriskuiper74@gmail.com>
2020-01-08 15:19:38 +0100

    feat(shop-store): add proper configuration

--- src/store/shop.js
+++ src/store/shop.js
@@ -37,19 +37,56 @@ export const state = () => ({
 })
 
 export const getters = {
-  amountOfItemsInShoppingBag(state) {
+  shoppingBagItemsQuantity(state) {
     return state.shoppingBag.length
+  },
+  shoppingBagTotalPrice(state) {
+    return state.shoppingBag.reduce((total, currentItem) => {
+      return total + currentItem.prijs
+    }, 0)
+  }
+}
+
+export const actions = {
+  addToShoppingBag(store, payload) {
+    if (payload.amount === 0) {
+      return store.commit(types.ADD_TO_SHOPPING_BAG, payload)
+    }
+
+    return store.commit(types.INCREMENT_ITEM_AMOUNT, payload)
+  },
+  removeFromShoppingBag(store, payload) {
+    if (payload.amount === 1) {
+      return store.commit(types.REMOVE_FROM_SHOPPING_BAG, payload)
+    }
+
+    return store.commit(types.DECREMENT_ITEM_AMOUNT, payload)
   }
 }
 
 export const mutations = {
   [types.ADD_TO_SHOPPING_BAG](state, payload) {
-    state.shoppingBag = [...state.shoppingBag, { ...payload.item, amount: 1 }]
+    state.shoppingBag = [...state.shoppingBag, { ...payload.item, amount: payload.amount }]
   },
   [types.REMOVE_FROM_SHOPPING_BAG](state, payload) {
-
+    state.shoppingBag = state.shoppingBag.filter(item => item.naam === payload.item.naam)
   },
   [types.INCREMENT_ITEM_AMOUNT](state, payload) {
+    state.shoppingBag = state.shoppingBag.map(item => {
+      if (item.naam === payload.item.naam) {
+        return { ...item, amount: item.amount++ }
+      }
 
+      return item
+    })
+  },
+  [types.DECREMENT_ITEM_AMOUNT](state, payload) {
+    state.shoppingBag = state.shoppingBag.map(item => {
+      if (item.naam === payload.item.naam) {
+        return { ...item, amount: item.amount-- }
+      }
+
+      return item
+    })
   }
 }
