ca5d5cd349d2e8c28ed967e721c9e1e7b0274536
Kris <kriskuiper74@gmail.com>
2020-01-13 11:08:25 +0100

    feat: create app table

---
+++ src/components/app-table/app-table.vue
@@ -0,0 +1,49 @@
+<template>
+  <table class="app-table">
+    <thead class="app-table__header">
+      <tr>
+        <th><abbr title="Aantal">Aant.</abbr></th>
+        <th>Omschrijving</th>
+        <th>Prijs</th>
+      </tr>
+    </thead>
+    <tbody class="app-table__body">
+      <tr
+        v-for="product in products"
+        :key="product.naam"
+      >
+        <td>{{ product.amount }}</td>
+        <td>{{ product.naam }}</td>
+        <td>{{ product.prijs }}</td>
+      </tr>
+    </tbody>
+    <tfoot class="app-table__footer">
+      <tr>
+        <td>Totaal:</td>
+        <td>{{ totalPrice }}</td>
+      </tr>
+    </tfoot>
+  </table>
+</template>
+
+<script>
+  export default {
+    props: {
+      products: {
+        type: Object,
+        required: true
+      }
+    },
+    computed: {
+      totalPrice() {
+        return this.products.reduce((totalPrice, currentItem) => {
+          return totalPrice + currentItem.prijs
+        }, 0)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
