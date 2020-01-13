6507be340f98d294239e27af99a12bdd9fe2ed3d
Kris <kriskuiper74@gmail.com>
2020-01-13 14:59:56 +0100

    feat(app-cart): implement substitute products in cart

--- src/components/app-cart/app-cart.vue
+++ src/components/app-cart/app-cart.vue
@@ -14,11 +14,16 @@
     <p class="app-cart__date">{{ formattedDate }}</p>
     <p class="app-cart__payment-company">Company name</p>
 
-    <app-table :products="shoppingBagProducts" />
+    <app-table
+      :products="shoppingBagProducts"
+      :total="shoppingBagTotalPrice"
+    />
 
     <h3 class="app-cart__title--special">Wow hiervan zou je ook...</h3>
-    <app-table :products="substituteProducts" />
-
+    <app-table
+      :products="substituteProducts"
+      :total="totalSubstitutePrice"
+    />
     <p class="app-cart__title--special">... kunnen betalen</p>
 
     <app-socials />
@@ -26,21 +31,65 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import { mapState, mapGetters } from 'vuex'
 
-  import CartTable from '../cart-table/cart-table'
+  import AppTable from '../app-table/app-table'
   import AppSocials from '../app-socials/app-socials'
 
   export default {
     components: {
-      CartTable,
+      AppTable,
       AppSocials
     },
     computed: {
       ...mapState({
-        selectedCompany: state => state.selectedCompany
-      })
+        selectedCompany: state => state.selectedCompany,
+        shoppingBag: state => state.shop.shoppingBag,
+        substituteItems: state => state.shop.substituteItems
+      }),
+      ...mapGetters({
+        shoppingBagTotalPrice: 'shop/shoppingBagTotalPrice'
+      }),
+      shoppingBagProducts() {
+        return Object.entries(this.shoppingBag)
+          .map(([name, object]) => {
+            const itemTotal = object.amount * object.prijs
+
+            return {
+              amount: object.amount,
+              naam: name,
+              prijs: itemTotal
+            }
+          })
+      },
+      substituteProducts() {
+        return this.substituteItems.map((item, index, self) => {
+          const part = this.shoppingBagTotalPrice / self.length
+
+          return {
+            ...item,
+            amount: Math.floor(part / item.prijs)
+          }
+        })
+      },
+      totalSubstitutePrice() {
+        return this.substituteProducts.reduce((total, current) => {
+          return total + (current.amount * current.prijs)
+        }, 0)
+      },
+      formattedDate() {
+        const today = new Date
+        const day = maybePrefixWithZero(today.getDay())
+        const month = maybePrefixWithZero(today.getMonth() + 1)
+        const year = today.getFullYear()
+
+        return `${day}-${month}-${year}`
+      }
     }
+  }
+
+  function maybePrefixWithZero(num) {
+    return num < 10 ? `0${num}` : num
   }
 </script>
 
