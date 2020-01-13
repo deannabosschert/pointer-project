c6b1c2b23f7edf36249d74a52126984c3faa1710
Kris <kriskuiper74@gmail.com>
2020-01-13 11:17:40 +0100

    feat: create app socials component

--- src/components/app-socials/app-socials.vue
+++ src/components/app-socials/app-socials.vue
@@ -1,10 +1,38 @@
 <template>
-
+  <section class="app-socials">
+    <h2 class="app-socials__title">Deel je ervaring en aankopen!</h2>
+    <div class="app-socials__buttons">
+      <button
+        @click="shareToTwitter"
+        class="app-socials__button"
+      >
+        <app-icon name="twitter" />
+        <span class="sr-only">Deel op Twitter</span>
+      </button>
+      <button
+        @click="shareToFacebook"
+        class="app-socials__button"
+      >
+        <app-icon name="facebook" />
+        <span class="sr-only">Deel op Facebook</span>
+      </button>
+    </div>
+  </section>
 </template>
 
 <script>
-  export default {
+  import AppIcon from '../app-icon/app-icon'
 
+  export default {
+    components: {
+      AppIcon
+    },
+    props: {
+      content: {
+        type: Object,
+        default: null
+      }
+    }
   }
 </script>
 
