5f870dfece104974b213a779a2f1eaffad39fc8b
Kris <kriskuiper74@gmail.com>
2020-01-13 08:45:11 +0100

    feat: create product-list component

---
+++ src/components/product-list/product-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="product-list">
+    <product-card
+      v-for="product in products"
+      :product="product"
+      :key="product.naam"
+    />
+  </div>
+</template>
+
+<script>
+  import ProductCard from '../product-card/product-card'
+
+  export default {
+    components: {
+      ProductCard
+    },
+    props: {
+      products: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .product-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .product-list .product-card {
+    flex: 0 0 100%;
+    margin-bottom: 1rem;
+  }
+</style>
