cb73f217dbf144e4976339ccfba778bb54d84462
Kris <kriskuiper74@gmail.com>
2020-01-13 11:17:49 +0100

    feat: create app-cart component

--- src/components/app-cart/app-cart.vue
+++ src/components/app-cart/app-cart.vue
@@ -1,10 +1,46 @@
 <template>
+  <aside class="app-cart">
+    <header class="app-cart__header">
+      <h2 class="app-cart__title">
+        <span class="sr-only">Bonnetje van: </span>
+        Selected company name
+      </h2>
+      <p class="app-cart__place">Plaats</p>
+      <ul class="app-cart__caretypes">
+        <li>Soort zorg</li>
+      </ul>
+    </header>
 
+    <p class="app-cart__date">{{ formattedDate }}</p>
+    <p class="app-cart__payment-company">Company name</p>
+
+    <app-table :products="shoppingBagProducts" />
+
+    <h3 class="app-cart__title--special">Wow hiervan zou je ook...</h3>
+    <app-table :products="substituteProducts" />
+
+    <p class="app-cart__title--special">... kunnen betalen</p>
+
+    <app-socials />
+  </aside>
 </template>
 
 <script>
-  export default {
+  import { mapState } from 'vuex'
 
+  import CartTable from '../cart-table/cart-table'
+  import AppSocials from '../app-socials/app-socials'
+
+  export default {
+    components: {
+      CartTable,
+      AppSocials
+    },
+    computed: {
+      ...mapState({
+        selectedCompany: state => state.selectedCompany
+      })
+    }
   }
 </script>
 
