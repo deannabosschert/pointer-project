9e4724eef9a8feaef41436c410348512e235ec63
Kris <kriskuiper74@gmail.com>
2020-01-13 15:47:08 +0100

    feat(default-layout): add meta tags for shareability

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -11,6 +11,18 @@
   export default {
     components: {
       AppHeader
+    },
+    head() {
+      return {
+        title: 'Shoppen met zorggeld',
+        meta: [
+          { property: 'og:url', content: 'https://www.pointer-project.netlify.com' },
+          { property: 'og:type', content: 'website' },
+          { property: 'og:title', content: 'Shoppen met zorggeld | Pointer' },
+          { property: 'og:description', content: 'Shop met de winst van zorgcowboys' },
+          { property: 'og:image', content: 'https://www.pointer-project.netlify.com' }
+        ]
+      }
     }
   }
 </script>
