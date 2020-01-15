b102eab7768dbf8b5190a0ef7897437be338c1ee
Kris <kriskuiper74@gmail.com>
2020-01-15 10:19:59 +0100

    feat: let shop be the shop page, another homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,57 @@
 <template>
-  <main class="shop-page">
-    <h1>This is the shop</h1>
-    <app-cart />
-    <product-list :products="shopItems" />
-    <nuxt-link to="/stats">Bekijk de cijfers</nuxt-link>
+  <main class="home-page">
+    <section class="home-page__intro">
+      <h1>Shoppen met zorggeld</h1>
+      <p>Zorgcoyboys zijn achterbakse ondernemers, gierige consultants en rijke managers. Dit zijn zorginstellingen die de afgelopen vijf jaar misbruik hebben gemaakt van het gebrek aan toezicht en de zorg alleen maar duurder maken.</p>
+    </section>
+    <section class="home-page__actions">
+      <h2>Zoek jouw zorginstelling</h2>
+      <form @submit.prevent="onSubmit">
+        <company-search required />
+        <year-selection
+          required
+          :selected-company="selectedCareCompany"
+        />
+      </form>
+    </section>
   </main>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import { mapGetters } from 'vuex'
+  import { SET_CARECOMPANIES } from '~/store/mutation-types'
 
-  import AppCart from '~/components/app-cart/app-cart'
-  import ProductList from '~/components/product-list/product-list'
+  import CompanySearch from '~/components/company-search/company-search'
+  import YearSelection from '~/components/year-selection/year-selection'
 
   export default {
     components: {
-      AppCart,
-      ProductList
+      CompanySearch,
+      YearSelection
     },
     computed: {
-      ...mapState({
-        shopItems: state => state.shop.shopItems
-      })
+      ...mapGetters({
+        careCompanies: 'careCompanies',
+        selectedCareCompany: 'selectedCareCompany',
+        selectedData: 'selectedData'
+      }),
+      async rawCompanies() {
+        const module = await import('~/static/data/pointer-raw')
+
+        return module.default
+      }
+    },
+    async mounted() {
+      if (!this.careCompanies) {
+        this.$store.commit(SET_CARECOMPANIES, { careCompanies: await this.rawCompanies })
+      }
+    },
+    methods: {
+
     }
   }
 </script>
+
+<style lang="scss">
+
+</style>
---
+++ src/pages/shop/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="shop-page">
+    <h1>This is the shop</h1>
+    <app-cart />
+    <product-list :products="shopItems" />
+    <nuxt-link to="/stats">Bekijk de cijfers</nuxt-link>
+  </main>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  import AppCart from '~/components/app-cart/app-cart'
+  import ProductList from '~/components/product-list/product-list'
+
+  export default {
+    components: {
+      AppCart,
+      ProductList
+    },
+    computed: {
+      ...mapState({
+        shopItems: state => state.shop.shopItems
+      })
+    }
+  }
+</script>
