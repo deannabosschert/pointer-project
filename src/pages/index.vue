52496995a872eac0a3c6b968f47c8ed34f1c6eab
Kris <kriskuiper74@gmail.com>
2020-01-15 11:50:43 +0100

    feat: finish home page styling

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,25 @@
 <template>
   <main class="home-page">
     <section class="home-page__intro">
-      <h1>Shoppen met zorggeld</h1>
+      <h1 class="home-page__title">Shoppen met zorggeld</h1>
       <p>Zorgcoyboys zijn achterbakse ondernemers, gierige consultants en rijke managers. Dit zijn zorginstellingen die de afgelopen vijf jaar misbruik hebben gemaakt van het gebrek aan toezicht en de zorg alleen maar duurder maken.</p>
     </section>
     <section class="home-page__actions">
-      <h2>Zoek jouw zorginstelling</h2>
+      <h2 class="home-page__actions-title">Zoek jouw zorginstelling</h2>
       <form @submit.prevent="onSubmit">
-        <company-search required />
+        <company-search
+          required
+          class="home-page__input"
+        />
         <year-selection
           required
           :selected-company="selectedCareCompany"
+          class="home-page__input"
         />
+        <p
+          v-if="selectedData"
+          class="home-page__budget"
+        >Jouw budget: <span>€{{ selectedData.winst.toLocaleString() }}</span></p>
         <div class="home-page__buttons">
           <button
             type="submit"
@@ -78,5 +86,56 @@
 </script>
 
 <style lang="scss">
+  .home-page {
+    padding: $spacing-default $spacing-medium;
+    background-image: url('~static/images/money-bag.png');
+    background-repeat: no-repeat;
+    background-position: 120% #{-$spacing-default};
+  }
 
+  .home-page__intro {
+    margin-bottom: $spacing-large;
+  }
+
+  .home-page__title {
+    margin-bottom: $spacing-large;
+  }
+
+  .home-page__actions-title {
+    margin-bottom: $spacing-default;
+  }
+
+  .home-page__input + .home-page__input {
+    margin-top: $spacing-small;
+  }
+
+  .home-page__budget {
+    width: 100%;
+    margin-top: $spacing-medium;
+    font-family: $font-stack-headings;
+    font-size: $font-size-default;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-weight: $font-weight-bold;
+    font-size: $font-size-medium;
+
+    & > span {
+      margin-left: $spacing-tiny;
+      font-weight: $font-weight-bold;
+      font-size: $font-size-large;
+    }
+  }
+
+  .home-page__buttons {
+    margin-top: $spacing-large;
+  }
+
+  .home-page .button {
+    margin-bottom: $spacing-small;
+  }
+
+  .home-page .button:last-child {
+    margin-bottom: 0;
+  }
 </style>
