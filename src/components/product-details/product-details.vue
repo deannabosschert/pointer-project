436480da6b72709e84895b1e2cc929afb4b4451f
Kris <kriskuiper74@gmail.com>
2020-01-15 12:29:20 +0100

    feat(product-details): fix product-details styling

--- src/components/product-details/product-details.vue
+++ src/components/product-details/product-details.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="product-details">
-    <h2>{{ product.naam }}</h2>
+    <h2 class="product-details__title">{{ product.naam }}</h2>
     <button
-      v-if="amount === 0"
       @click="$emit('add-to-cart-click')"
       class="product-details__button"
     >
@@ -11,7 +10,7 @@
     </button>
 
     <div
-      v-else
+      v-if="amount > 0"
       class="product-details__controls"
     >
       <button
@@ -21,7 +20,7 @@
         <span class="sr-only">Verwijder één uit winkelmandje</span>
         <app-icon name="min_black" />
       </button>
-      <p>{{ amount || 'onbekend' }}</p>
+      <p class="product-details__amount">{{ amount || 'onbekend' }}</p>
       <button
         @click="$emit('add-to-cart-click')"
         class="product-details__button"
@@ -54,12 +53,55 @@
 </script>
 
 <style lang="scss">
+  $product-details-height: 36px;
+
   .product-details,
   .product-details__controls {
     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: $spacing-small;
+    background: $color-darkest;
+    color: $color-white;
+  }
+
+  .product-details {
+    position: relative;
+  }
+
+  .product-details__controls {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba($color-darkest, .8);
+  }
+
+  .product-details__amount {
+    background: $color-white;
+    color: $color-darkest;
+    height: 100%;
+    min-width: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .product-details__title {
+    font-family: $font-stack-body;
+    font-size: $font-size-default;
+    font-weight: $font-weight-default;
   }
 
   .product-details__button {
-    font-size: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 1.5em;
+    width: 1.5em;
+    border-radius: 50%;
+    font-size: $font-size-default;
+    background: $color-highlight-yellow;
   }
 </style>
