6e4e29718beb0a5220e7fe29c848a19750c37c8b
Kris <kriskuiper74@gmail.com>
2020-01-15 13:51:42 +0100

    feat: set up cart page

---
+++ src/pages/cart/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="cart-page">
+    <app-cart :selectedCompany="selectedCareCompany" />
+    <nuxt-link
+      to="/stats"
+      class="button"
+    >
+      Check de cijfers
+    </nuxt-link>
+  </main>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  import AppCart from '~/components/app-cart/app-cart'
+
+  export default {
+    components: {
+      AppCart
+    },
+    computed: {
+      ...mapGetters({
+        selectedCareCompany: 'selectedCareCompany'
+      })
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
