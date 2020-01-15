afd678a004c074c7abe0d429423108936d1d50ba
Kris <kriskuiper74@gmail.com>
2020-01-15 12:01:41 +0100

    feat(shop-page): add page styling

--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -1,14 +1,18 @@
 <template>
   <main class="shop-page">
-    <h1>This is the shop</h1>
-    <app-cart />
+    <h1 class="sr-only">Shop met de winst</h1>
+    <p class="shop-page__intro-text">Wat koop jij met de winst van {{ selectedData.naam }}?</p>
+    <p class="shop-page__budget" v-if="selectedData">
+      <span class="shop-page__budget-intro">Jouw budget:</span>
+      <span class="shop-page__budget-budget">€{{ selectedData.winst.toLocaleString() }}</span>
+    </p>
     <product-list :products="shopItems" />
     <nuxt-link to="/stats">Bekijk de cijfers</nuxt-link>
   </main>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import { mapState, mapGetters } from 'vuex'
 
   import AppCart from '~/components/app-cart/app-cart'
   import ProductList from '~/components/product-list/product-list'
@@ -19,9 +23,40 @@
       ProductList
     },
     computed: {
+      ...mapGetters({
+        selectedData: 'selectedData'
+      }),
       ...mapState({
         shopItems: state => state.shop.shopItems
       })
     }
   }
 </script>
+
+<style lang="scss">
+  .shop-page {
+    padding: $spacing-default $spacing-medium;
+  }
+
+  .shop-page__intro-text {
+    margin-bottom: $spacing-small;
+    text-align: center;
+  }
+
+  .shop-page__budget {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: $spacing-default;
+    text-align: center;
+    font-family: $font-stack-headings;
+  }
+
+  .shop-page__budget-intro {
+    font-size: $font-size-medium;
+  }
+
+  .shop-page__budget-budget {
+    font-size: $font-size-large;
+    font-weight: $font-weight-bold;
+  }
+</style>
