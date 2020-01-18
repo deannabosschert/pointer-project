7009518bac179d12eeaa592c04e6de351b8e9ace
deannabosschert <deanna.bosschert@gmail.com>
2020-01-18 20:46:33 +0100

    feat(bar-chart): added options of x-axes and y-axes

--- src/components/bar-chart/bar-chart.js
+++ src/components/bar-chart/bar-chart.js
@@ -3,7 +3,7 @@ import { Bar } from "vue-chartjs"
 export default {
   extends: Bar,
   props: {
-    data: ['data', 'options'],
+    data: ["data", "options"],
     selectedData: {
       type: Object,
       required: true
@@ -33,29 +33,84 @@ export default {
     }
   },
   mounted() {
-    let nederlandL = this.dutchData.naam
-    let bedrijfL = this.selectedData.naam
+    let nederland = this.dutchData.naam
+    let bedrijf = this.selectedData.naam
 
     let nederlandV = this.dutchData.percentageWinst
     let gemiddeldV = 5
+    let test = -4
     let bedrijfV = this.selectedData.percentageWinst
 
-    this.renderChart({
-      labels: [nederlandL, "gemiddeld", bedrijfL],
-      datasets: [
-        {
-          label: "Winstpercentage",
-          backgroundColor: ["#1beaae", "rgba(255, 255, 255, 0)", "#6b38e8"],
-          borderColor: [
-        '',
-        '#f65645',
-        ''
-      ],
-    borderWidth: [0, 2, 0],
-          data: [nederlandV, gemiddeldV, bedrijfV]
-        }
-      ],
+    this.renderChart(
+      {
+        labels: [nederland, "gemiddeld", bedrijf],
+        datasets: [
+          {
+            data: [nederlandV, gemiddeldV, test],
+            label: "Winstpercentage",
+            backgroundColor: ["#1beaae", "rgba(255, 255, 255, 0)", "#6b38e8"],
+            borderColor: ["", "#f65645", ""],
+            borderWidth: [0, 2, 0]
+          }
+        ]
+      },
+      {
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function showPercentage() {
+            var chartInstance = this.chart
+            ctx = chartInstance.ctx
+            ctx.textAlign = "center"
+            ctx.textBaseline = "bottom"
 
-    })
+            this.data.datasets.forEach(function(dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i)
+              meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index]
+                ctx.fillText(data, bar._model.x, bar._model.y - 5)
+              })
+            })
+          }
+        },
+        responsive: false,
+        scales: {
+          yAxes: [
+            {
+              display: true,
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                // max: Math.max(... datasets[0].data) + 10,
+                display: true,
+                beginAtZero: true
+              }
+            }
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                beginAtZero: true,
+                display: true
+              }
+            }
+          ]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        },
+        maintainAspectRatio: false,
+        height: 200
+      }
+    )
   }
 }
