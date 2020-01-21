92f4abe7e55ed49c7e005b1c5afa244dac7f5922
Kris <kriskuiper74@gmail.com>
2020-01-21 12:49:40 +0100

    feat: create and implement app-toggle

---
+++ src/components/app-toggle/app-toggle.vue
@@ -0,0 +1,69 @@
+<template>
+  <form class="app-toggle" @change="onChange">
+    <input
+      type="radio"
+      id="compare-profit-input"
+      class="app-toggle__radio off-screen"
+      name="app-toggle-input"
+      value="compare"
+      selected
+    >
+    <label
+      for="compare-profit-input"
+      class="app-toggle__label"
+    >
+      Vergelijk omzet
+    </label>
+    <input
+      type="radio"
+      id="see-profit-input"
+      class="app-toggle__radio off-screen"
+      name="app-toggle-input"
+      value="see"
+    >
+    <label
+      for="see-profit-input"
+      class="app-toggle__label"
+    >
+      Verdeling omzet
+    </label>
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onChange(event) {
+        if (event.target.value === 'compare') {
+          return this.$emit('compare-click')
+        }
+
+        return this.$emit('see-click')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  $app-toggle-border-radius: 10px;
+  $app-toggle-label-border-radius: 7px;
+
+  .app-toggle {
+    display: flex;
+    padding: $spacing-tiny;
+    border-radius: $app-toggle-border-radius;
+    background: $color-white;
+  }
+
+  .app-toggle__label {
+    flex: 0 0 50%;
+    padding: $spacing-small 0;
+    border-radius: $app-toggle-label-border-radius;
+    background: $color-white;
+    text-align: center;
+  }
+
+  .app-toggle__radio:checked + .app-toggle__label {
+    background: $color-highlight-yellow;
+  }
+</style>
--- src/components/charts/charts.vue
+++ src/components/charts/charts.vue
@@ -1,62 +1,73 @@
 <template>
   <main>
-
-    <div>
-    <h2 class="chart-title">Percentage winst</h2>
-    <bar-chart
-      v-if="selectedData && selectedDutchData"
-      :selected-data="selectedData"
-      :dutch-data="selectedDutchData"
-      property="percentageWinst"
-      title="Percentage winst"
-      gemiddelde = '4'
+    <app-toggle
+      @compare-click="choice = 'compare'"
+      @see-click="choice = 'see'"
     />
+    <div v-if="choice === 'compare'">
+      <h2 class="chart-title">Percentage winst</h2>
+      <bar-chart
+        v-if="selectedData && selectedDutchData"
+        :selected-data="selectedData"
+        :dutch-data="selectedDutchData"
+        property="percentageWinst"
+        title="Percentage winst"
+        gemiddelde = '4'
+      />
 
-    <h2 class="chart-title">Percentage loon </h2>
-    <bar-chart
-      v-if="selectedData && selectedDutchData"
-      :selected-data="selectedData"
-      :dutch-data="selectedDutchData"
-      property="percentageLoon"
-      title="Percentage loon"
-      gemiddelde = '40'
+      <h2 class="chart-title">Percentage loon </h2>
+      <bar-chart
+        v-if="selectedData && selectedDutchData"
+        :selected-data="selectedData"
+        :dutch-data="selectedDutchData"
+        property="percentageLoon"
+        title="Percentage loon"
+        gemiddelde = '40'
 
-    />
+      />
 
-    <h2 class="chart-title">Omzet per FTE</h2>
-    <bar-chart
-      v-if="selectedData && selectedDutchData"
-      :selected-data="selectedData"
-      :dutch-data="selectedDutchData"
-      property="omzetPerFte"
-      title="Omzet per FTE"
-      gemiddelde = '125000'
-    />
-  </div>
+      <h2 class="chart-title">Omzet per FTE</h2>
+      <bar-chart
+        v-if="selectedData && selectedDutchData"
+        :selected-data="selectedData"
+        :dutch-data="selectedDutchData"
+        property="omzetPerFte"
+        title="Omzet per FTE"
+        gemiddelde = '125000'
+      />
+    </div>
 
-  <div>
-    <stacked-bar-chart
-      v-if="selectedData && selectedDutchData"
-      :selected-data="selectedData"
-      :dutch-data="selectedDutchData"
-      property="omzet"
-      title="Stacken met die barzz"
-    />
-  </div>
+    <div v-else-if="choice === 'see'">
+      <stacked-bar-chart
+        v-if="selectedData && selectedDutchData"
+        :selected-data="selectedData"
+        :dutch-data="selectedDutchData"
+        property="omzet"
+        title="Stacken met die barzz"
+      />
+    </div>
   </main>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import { SET_CARECOMPANIES, SET_DUTCH_DATA } from '~/store/mutation-types'
+
+import AppToggle from '~/components/app-toggle/app-toggle'
 import BarChart from '~/components/bar-chart/bar-chart'
 import StackedBarChart from '~/components/stacked-bar-chart/stacked-bar-chart'
 
 
 export default {
   components: {
+    AppToggle,
     BarChart,
     StackedBarChart
+  },
+  data() {
+    return {
+      choice: 'compare'
+    }
   },
   computed: {
     ...mapGetters({
@@ -74,7 +85,7 @@ export default {
   margin-top: $spacing-large;
 }
 
-bar-chart{
+bar-chart {
   overflow: hidden;
 }
 canvas {
