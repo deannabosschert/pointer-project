47f991eae8116d0047c03d3a0a87221dcc8a4afd
deannabosschert <deanna.bosschert@gmail.com>
2020-01-21 21:52:44 +0100

    style(charts): format

--- src/components/charts/charts.vue
+++ src/components/charts/charts.vue
@@ -6,12 +6,16 @@
     />
     <h2 class="app-charts_companyName">{{ selectedData.naam }}</h2>
     <div class="app-charts_companyInfo">
-    <p><app-icon name="location_black" />{{ selectedData.plaats }} </p>
-    <p><app-icon name="thuiszorg_black" />{{ selectedData.soortenZorg[0] }}</p>
-  </div>
+      <p>
+        <app-icon name="location_black" />{{ selectedData.plaats }}
+      </p>
+      <p>
+        <app-icon name="thuiszorg_black" />{{ selectedData.soortenZorg[0] }}
+      </p>
+    </div>
 
     <div v-if="choice === 'compare'">
-      <h2 class="app-charts-title">Percentage winst</h2>
+      <h2 class="app-charts_title">Percentage winst</h2>
       <p>
         Het winstpercentage is de omzet gedeeld door de winst keer honderd. Dit
         is verdacht boven de 4%.
@@ -25,7 +29,7 @@
         average="4"
       />
 
-      <h2 class="app-charts-title">Percentage loon</h2>
+      <h2 class="app-charts_title">Percentage loon</h2>
       <p>
         Het percentage loon is het totale aantal personeelskosten gedeeld door
         de omzet. Een normaal percentage ligt rond de 40%.
@@ -39,7 +43,7 @@
         average="40"
       />
 
-      <h2 class="app-charts-title">Omzet per FTE</h2>
+      <h2 class="app-charts_title">Omzet per FTE</h2>
       <p>
         Omzet per FTE geeft aan hoeveel omzet een full-time medewerker van een
         bedrijf oplevert. Het is verdacht als een zorgbedrijf een hoge FTE heeft
@@ -57,7 +61,7 @@
     </div>
 
     <div v-else-if="choice === 'see'">
-      <h2 class="app-charts-title">Verdeling van de omzet</h2>
+      <h2 class="app-charts_title">Verdeling van de omzet</h2>
       <stacked-bar-chart
         v-if="selectedData && selectedDutchData"
         :selected-data="selectedData"
@@ -70,13 +74,13 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { SET_CARECOMPANIES, SET_DUTCH_DATA } from "~/store/mutation-types";
+import { mapGetters } from "vuex"
+import { SET_CARECOMPANIES, SET_DUTCH_DATA } from "~/store/mutation-types"
 
-import AppIcon from '../app-icon/app-icon'
-import AppToggle from "~/components/app-toggle/app-toggle";
-import BarChart from "~/components/bar-chart/bar-chart";
-import StackedBarChart from "~/components/stacked-bar-chart/stacked-bar-chart";
+import AppIcon from "../app-icon/app-icon"
+import AppToggle from "~/components/app-toggle/app-toggle"
+import BarChart from "~/components/bar-chart/bar-chart"
+import StackedBarChart from "~/components/stacked-bar-chart/stacked-bar-chart"
 
 export default {
   components: {
@@ -88,7 +92,7 @@ export default {
   data() {
     return {
       choice: "compare"
-    };
+    }
   },
   computed: {
     ...mapGetters({
@@ -97,24 +101,19 @@ export default {
     })
   },
   async mounted() {}
-};
+}
 </script>
 
 <style lang="scss">
-.app-charts-companyName {
+.app-charts_companyName {
   margin-top: $spacing-large;
 }
 
-.app-charts_companyInfo{
+.app-charts_companyInfo {
   display: flex;
   padding: $spacing-tiny;
 }
 
-
-//
-// .app-charts__icon {
-//   display: inline-flex;
-//   margin-left: auto;
-//   padding: 1rem;
-// }
+.app-charts_title {
+}
 </style>
